7c7719964a6123dce9b585081a54c1ab7c3c0441
tavaresjsvn <tavaresjsvn@gmail.com>
2025-06-30 11:29:15 -0300

    test: adiciona estrutura base de testes gerada pelo Spring Initializr

---
+++ src/test/java/com/projeto/catalago_livros/CatalagoLivrosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.catalago_livros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalagoLivrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
